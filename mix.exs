33eb474c2708277f8d78c8774cde1bd4a496f2fa
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-08 04:55:51 +0900

    build: install req and floki

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule Elixshot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:req, "~> 0.3"},
+      {:floki, "~> 0.34"}
     ]
   end
 end
